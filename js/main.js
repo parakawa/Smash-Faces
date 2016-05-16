6d2979375049f2ac5ecc1008e22866478466c5d2
parakawa <patyna127@gmail.com>
2016-05-16 15:49:21 -0500

    falta hacer las sedes

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 //$("#coder").attr("src","fotos/Ana Maria Barbosa.JPG")
 
   var puntaje=0;
+  var tamAre=arequipa.length
  $(function(){
   
   $(".comprobar").on("click",comprobar)
@@ -12,8 +13,8 @@
 function comprobar(){
   var foto=$("#coder").attr("src")
   var nombre=$(".nombre").val()
- 
-  var tamAre=arequipa.length
+  var sedes=$("#sedes").val()
+  
   var img;
   var intento=6 ;
   
@@ -22,27 +23,37 @@ function comprobar(){
     img="fotos/arequipa/"+arequipa[i].image
     if(foto==img)
       if(nombre==arequipa[i].name){ 
-          $(".mensaje").text("Hey, conoces bien a la chica!") 
+          $(".mensaje").text("Hey, conoces bien a "+arequipa[i].name+sedes) 
           puntaje=puntaje+5
           $("#puntos").html(puntaje)
-          $("#coder").attr("src","fotos/arequipa/Ana Maria Barbosa.JPG")
+          clean()
+          cargarImagen()
           }   
       else{
-        if(intento<=5){ 
-        
-        $(".mensaje").text("Tienes otro intento"+" Un secreto: su nombre es "+arequipa[i].name)
-        intento=intento+1
-        }
-        else{
-          puntaje=puntaje-1
-          $(".mensaje").text("Lo sentimos, ya gastaste todos los intentos posibles")
-          $("#puntos").html(puntaje)
+          if(intento<=5){ 
+          
+          $(".mensaje").text("Tienes otro intento"+" Un secreto: su nombre es "+arequipa[i].name)
+          intento=intento+1
+          }
+          else{
+            puntaje=puntaje-1
+            $(".mensaje").text("Lo sentimos, ya gastaste todos los intentos posibles")
+            $("#puntos").html(puntaje)
+            cargarImagen()
         }
 
       }
     }
 }
 
+function cargarImagen(){
+  $("#coder").attr("src","fotos/arequipa/"+arequipa[Math.floor((Math.random() * tamAre) + 0)].image)
+}
+
+function clean(){
+  $(".nombre").val("")
+  $(".nombre").focus()
+}
 
 
 
