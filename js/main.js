e4b91f95917a62d755b7dd8a3a548382cee11f4f
parakawa <patyna127@gmail.com>
2016-05-16 18:57:43 -0500

    sedes, intento

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@
 
   var puntaje=0;
   var tamAre=arequipa.length
+  var foto=$("#coder").attr("src")
+  var img;
  $(function(){
   
   $(".comprobar").on("click",comprobar)
@@ -11,48 +13,62 @@
  })
 
 function comprobar(){
-  var foto=$("#coder").attr("src")
-  var nombre=$(".nombre").val()
+  
+  
   var sedes=$("#sedes").val()
   
-  var img;
-  var intento=6 ;
   
-
-  for(i=0; i<tamAre;i++){ 
-    img="fotos/arequipa/"+arequipa[i].image
-    if(foto==img)
-      if(nombre==arequipa[i].name){ 
-          $(".mensaje").text("Hey, conoces bien a "+arequipa[i].name+sedes) 
-          puntaje=puntaje+5
-          $("#puntos").html(puntaje)
-          clean()
-          cargarImagen()
-          }   
-      else{
-          if(intento<=5){ 
+  
+  switch(sedes){
+    case "mexico":
+          main(mexico,sedes) 
           
-          $(".mensaje").text("Tienes otro intento"+" Un secreto: su nombre es "+arequipa[i].name)
-          intento=intento+1
-          }
-          else{
-            puntaje=puntaje-1
-            $(".mensaje").text("Lo sentimos, ya gastaste todos los intentos posibles")
-            $("#puntos").html(puntaje)
-            cargarImagen()
-        }
+          break;
+    default: 
+          main(arequipa,sedes) 
+           }
 
-      }
-    }
+
 }
 
 function cargarImagen(){
-  $("#coder").attr("src","fotos/arequipa/"+arequipa[Math.floor((Math.random() * tamAre) + 0)].image)
+  
 }
 
 function clean(){
   $(".nombre").val("")
   $(".nombre").focus()
+}
+
+function main(arraySede,sede){
+      var nombre=$(".nombre").val()
+      var intento=0 ;
+       for(i=0; i<arraySede.length;i++){ 
+              img="fotos/"+sede+"/"+arraySede[i].image
+              if(foto==img)
+                if(nombre==arraySede[i].name){ 
+                    $(".mensaje").text("Hey, conoces bien a "+arraySede[i].name+sede) 
+                    puntaje=puntaje+5
+                    $("#puntos").html(puntaje)
+                    clean()
+                    $("#coder").attr("src","fotos/"+sede+"/"+arraySede[Math.floor((Math.random() * arraySede.length) + 0)].image)
+                    }   
+                else{
+                    if(intento<=5){ 
+                    
+                    $(".mensaje").text("Tienes otro intento"+" Un secreto: su nombre es "+arraySede[i].name)
+                    intento=intento+1
+                    }
+                    else{
+                      puntaje=puntaje-1
+                      $(".mensaje").text("Lo sentimos, ya gastaste todos los intentos posibles")
+                      $("#puntos").html(puntaje)
+                      $("#coder").attr("src","fotos/"+sede+"/"+arraySede[Math.floor((Math.random() * arraySede.length) + 0)].image)
+                  }
+
+                }
+              
+             }
 }
 
 
